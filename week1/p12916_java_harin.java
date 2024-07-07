ae947bcd8e2e4e66940bddeadc876e9c7e1538a2
harin1212 <harin2950@naver.com>
2024-07-07 15:52:28 +0900

    [12916] 문자열 내 p와 y의 개수

---
+++ week1/p12916_java_harin.java
@@ -0,0 +1,27 @@
+/**
+ * 문자열 내 p와 y의 개수
+ */
+public class p12916_java_harin {
+    static class Solution {
+        public boolean solution(String s) {
+            int pctn = 0;
+            int yctn = 0;
+            boolean answer = true;
+            s = s.toLowerCase(); // 소문자 변환
+
+            for (char x: s.toCharArray()){ // 문자 분리하여 배열 만들어줌
+                if (x=='p') pctn++;
+                else if (x=='y') yctn++;
+            }
+            answer = (pctn == yctn) ? true : false;
+
+            return answer;
+        }
+    }
+    public class Main {
+        public static void main(String[] ars) {
+            Solution s = new Solution();
+            s.solution("pPoooyY");
+        }
+    }
+}
