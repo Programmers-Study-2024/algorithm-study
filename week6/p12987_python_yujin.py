cde63adf84203d52d4726ccddad8c6f594590dd8
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-08-13 19:51:44 +0900

    [12987] 숫자 게임

---
+++ week6/p12987_python_yujin.py
@@ -0,0 +1,12 @@
+# 숫자 게임
+
+def solution(A, B):
+    answer = 0
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    i = 0
+    for a in A:
+        if a < B[i]:
+            answer += 1
+            i += 1
+    return answer
