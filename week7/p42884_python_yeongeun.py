a8ce701187d02796f0f4a78bf52cc9255e80abda
hanni K <70632327+yyeongeun@users.noreply.github.com>
2024-08-19 15:42:53 +0900

    단속카메라

--- week7/p42884_python_yeongeun.py
+++ week7/p42884_python_yeongeun.py
@@ -2,7 +2,6 @@
 
 def solution(routes):
     routes.sort(key = lambda x:x[1]) # 진출시점 기준으로 정렬
-    # 진출지점에 대해 정렬해야, 진출지점에 카메라가 있다고 했을 때, 그 다음 진입지점이 진출지점보다 앞서면, 반드시 그 카메라에 단속된다고 장담할 수 있다.
     key = -30001 # 최저 key
     answer = 0
     
