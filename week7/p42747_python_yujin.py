993d0620dba98284ed3a5cf365b32c248af6c81d
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-08-19 16:41:56 +0900

    [42747] H-Index

---
+++ week7/p42747_python_yujin.py
@@ -0,0 +1,10 @@
+# H-Index
+
+def solution(citations):
+    answer = 0
+    citations.sort()
+    
+    for i in range(len(citations)):
+        if citations[i] >= len(citations) - i:
+            return len(citations) - i
+    return answer
