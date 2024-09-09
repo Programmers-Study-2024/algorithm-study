cf44084ccb1414020b7a7182dbfdaad6045d9fba
donghui <louisp0215@gmail.com>
2024-09-09 14:18:18 +0900

    [131124] 그룹별 조건에 맞는 식당 목록 출력하기

---
+++ week10/p131124_sql_donghui.sql
@@ -0,0 +1,2 @@
+SELECT m.MEMBER_NAME,r.REVIEW_TEXT,DATE_FORMAT(r.REVIEW_DATE,'%Y-%m-%d') as REVIEW_DATE  from MEMBER_PROFILE as m join REST_REVIEW as r on m.MEMBER_ID = r.MEMBER_ID where r.MEMBER_ID = (select MEMBER_ID from REST_REVIEW group by MEMBER_ID  order by count(REVIEW_SCORE) desc limit 1)
+order by r.REVIEW_DATE asc, r.REVIEW_TEXT;
