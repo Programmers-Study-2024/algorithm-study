ba03ff7232f5a200cc179f905c954cc5132502ec
donghui <louisp0215@gmail.com>
2024-09-15 23:44:17 +0900

    [340207] [PCCE 기출문제] 1번 / 문자 출력

---
+++ week11/p340207_java_donghui.java
@@ -0,0 +1,10 @@
+package week11;
+
+public class p340207_java_donghui {
+    public static void main(String[] args) {
+        String message = " Let 's go! ";
+
+        System.out.println("3\n 2\n 1");
+                System.out.println(message);
+    }
+}
