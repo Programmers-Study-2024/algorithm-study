9da7b87d3514ce8756578dc9953dabe6830d907b
harin1212 <harin2950@naver.com>
2024-09-16 11:51:18 +0900

    [42586] 기능개발

---
+++ week11/p42586_java_harin.java
@@ -0,0 +1,37 @@
+package week11;
+import java.util.*;
+
+public class p42586_java_harin {
+    /**
+     * 고득점 kit - 스택/큐
+     *  기능개발
+     */
+    class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            Queue<Integer> queue = new LinkedList<>();
+
+            // 작업 일수 큐에 담음
+            for (int i=0; i<progresses.length; i++) {
+                queue.add((int) Math.ceil((100.0 - progresses[i]) / speeds[i]));
+            }
+
+            while (!queue.isEmpty()){
+                int minDays = queue.poll();
+                int count = 1;
+                while (!queue.isEmpty() && queue.peek() <= minDays){
+                    queue.poll();
+                    count++;
+                }
+                arr.add(count);
+            }
+
+            int[] answer = new int[arr.size()];
+            for (int i=0; i<arr.size(); i++){
+                answer[i] = arr.get(i);
+            }
+
+            return answer;
+        }
+    }
+}
