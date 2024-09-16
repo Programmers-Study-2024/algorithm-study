16bf3c39dc6cadc57f4c469fb5ff85216d6b2389
donghui <louisp0215@gmail.com>
2024-09-16 17:42:23 +0900

    [42584] 주식가격

--- week11/p42584_java_donghui.java
+++ week11/p42584_java_donghui.java
@@ -6,30 +6,23 @@ public class p42584_java_donghui {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
 
+        // 테스트5 시간초과 남 -> 스택2개 이용했을 때.. 1개로 하면가능 다시 풀어야 함
         Stack<Integer> stack = new Stack<>();       //
 
-        for (int i = prices.length - 1; i >= 0; i--) {
-            int p = prices[i];
+        for (int i = 0; i < prices.length; i++) {
+            answer[i] = prices.length - 1 - i;      // 가격이 떨어지지않을 때 최대 기간
             while (!stack.isEmpty()) {
-
-                if (prices[stack.peek()] >= prices[i]) break;
+                if (prices[stack.peek()] <= prices[i]) break;        // 가격이 안떨어지면 그냥 나가기
 
                 int idx = stack.pop();
-
-                answer[i] = prices.length - 1 - idx;
-
+                answer[idx] = i - idx;
             }
 
             stack.push(i);
 
         }
-        // stack 에 남아있는 수들은 자신보다 작은 수가 없는 것들!
-        while (!stack.isEmpty()) {
-            int index = stack.pop();
-            answer[index] = prices.length - 1 - index;
-        }
 
+        //  System.out.println(stack.size());
         return answer;
     }
-
 }
