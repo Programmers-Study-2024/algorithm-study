7639bb2cc0da04b31f68f99e6f92ada5a61649e7
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-09-29 17:45:24 +0900

    [42861] 섬 연결하기

---
+++ week13/p42861_python_yujin.py
@@ -0,0 +1,19 @@
+# 섬 연결하기 
+
+def solution(n, costs):
+    answer = 0
+    
+    # 비용순으로 정렬
+    costs.sort(key=lambda x:x[2])
+    V=set([costs[0][0]])
+    
+    while len(V) != n :
+        for E in costs:
+            if E[0] in V and E[1] in V :
+                continue
+            if E[0] in V or E[1] in V :
+                V.add(E[0])
+                V.add(E[1])
+                answer+=E[2]
+                break  
+    return answer
