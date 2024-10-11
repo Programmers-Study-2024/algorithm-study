a9232c68b392853b029f2e2a691c6a2cd3a99336
hanni K <70632327+yyeongeun@users.noreply.github.com>
2024-10-11 19:17:09 +0900

    2개 이하로 다른 비트

---
+++ week15/p77885_python_yeongeun.py
@@ -0,0 +1,17 @@
+# 2개 이하로 다른 비트
+
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        bin_num = list('0' + bin(num)[2:])
+        
+        # 가장 오른쪽 0을 찾아서 1로 바꿔주기
+        idx = ''.join(bin_number).rfind('0')
+        bin_num[idx] = '1'
+        
+        # 홀수인 경우
+        # 7 = 0111, 1111보단 1011이 더 작은 값
+        if num % 2 == 1:
+            bin_num[idx+1] = '0'
+        answer.append(int(''.join(bin_num),2))
+    return answer
