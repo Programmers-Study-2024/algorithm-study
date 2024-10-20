982421bddbfe13e04c854c84554d7034198f6bab
hanni K <70632327+yyeongeun@users.noreply.github.com>
2024-10-20 20:57:07 +0900

    폰켓몬

---
+++ week16/p1845_python_yeongeun.py
@@ -0,0 +1,9 @@
+# 폰켓몬
+
+def solution(nums):    
+    count = {}
+    for n in nums: # 겹치는 포켓몬 몇개인지는 중요하지 않음
+        count[n] = 1
+
+    # n//2마리 포켓몬 다 다르게 고르거나, 겹치면 안겹치게 고르기
+    return min(len(count),len(nums)//2)
